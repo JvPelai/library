b34e082bb704104ffe028ea43ccfb2a0be51c065
jvpelai <jv.pelai@gmail.com>
2020-08-25 21:50:27 -0300

    dropdown menu & main functions

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,19 +1,60 @@
 "use strict";
 
+var library = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = "not yet read";
 
-  if (read) {
+  if (read == "yes") {
     this.read = "already read";
   }
 
   this.info = function () {
-    return title + " by " + author + ", " + pages + " pages, " + this.read;
+    console.log(title + " by " + author + ", " + pages + " pages, " + this.read);
   };
 }
 
-var faust = new Book('Faust', "Goethe", 478, false);
-console.log(faust.info());
+function addBook() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function addToLibrary() {
+  var newTitle = document.querySelector('.newTitle').value;
+  var newAuthor = document.querySelector('.newAuthor').value;
+  var pages = document.querySelector('.pages').value;
+  var read;
+  var radios = document.getElementsByName("read");
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = radios[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var r = _step.value;
+
+      if (r.checked) {
+        read = r.value;
+      }
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  var newBook = new Book(newTitle, newAuthor, pages, read);
+  library.push(newBook);
+  return newBook;
+}
--- script.js
+++ script.js
@@ -1,16 +1,34 @@
+var library = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = "not yet read";
-  if (read) {
+  if (read == "yes") {
     this.read = "already read";
   }
   this.info = function () {
-    return title + " by " + author + ", " + pages + " pages, " + this.read
+    console.log(title + " by " + author + ", " + pages + " pages, " + this.read)
   }
 }
 
-const faust = new Book('Faust', "Goethe", 478, false);
+function addBook() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-console.log(faust.info());
+function addToLibrary() {
+  let newTitle = document.querySelector('.newTitle').value
+  let newAuthor = document.querySelector('.newAuthor').value
+  let pages = document.querySelector('.pages').value
+  let read;
+  let radios = document.getElementsByName("read");
+  for (let r of radios) {
+    if (r.checked) {
+      read = r.value
+    }
+  }
+  let newBook = new Book(newTitle, newAuthor, pages, read);
+  library.push(newBook);
+  return newBook;
+}
