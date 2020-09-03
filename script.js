97ee2e916abfdec0fc1f487916312cafb189b3c5
jvpelai <jv.pelai@gmail.com>
2020-09-02 21:52:03 -0300

    renderBook function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var library = [];
+const lib_bookshelf = document.getElementById('lib-bookshelf');
+
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -31,4 +33,20 @@ function addToLibrary() {
   let newBook = new Book(newTitle, newAuthor, pages, read);
   library.push(newBook);
   return newBook;
+}
+
+//Display books
+
+function renderBooks() {
+  lib_bookshelf.innerHTML = '';
+  library.forEach(function (item, index) {
+    renderBook(item, index);
+  });
+}
+
+function renderBook(item, index) {
+  const book_title = item.title;
+  const book_author = item.author;
+  const book_pages = item.pages;
+  const read_stat = item.read;
 }
